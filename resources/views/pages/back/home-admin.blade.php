148d6bc72f8903bb1edb04470751f811b34f2ff4
zilva149 <114057257+zilva149@users.noreply.github.com>
2023-02-05 17:22:11 +0200

    Full admin home page design

--- resources/views/pages/back/home-admin.blade.php
+++ resources/views/pages/back/home-admin.blade.php
@@ -16,8 +16,8 @@
             class='bg-slate-400 hover:bg-slate-500' />
     </section>
 
-    <div class="mb-6 flex flex-col gap-6 2xl:flex-row">
-        <section class="w-full p-6 bg-white rounded-md shadow-md flex flex-col gap-4 2xl:w-2/3">
+    <div class="mb-6 flex flex-col gap-6 2xl:flex-row dark:bg-dark-eval-0 dark:text-gray-200">
+        <section class="w-full p-6 bg-white rounded-md shadow-md flex flex-col gap-6 2xl:w-2/3 dark:bg-dark-eval-1">
             <div class="flex gap-4 justify-center items-center">
                 <h3 class="text-xl font-semibold text-center">Užsakymai</h3>
                 <a href="{{ route('admin-orders') }}"
@@ -31,7 +31,13 @@
             <x-back.home-order name='Elena Danienė' orderID='3' status='approved' />
             <x-back.home-order name='Marius Būžinskas' orderID='5' status='approved' />
         </section>
-        <section class="w-full p-6 bg-white rounded-md shadow-md flex flex-col gap-4 2xl:w-1/3">
+        <section class="w-full p-6 bg-white rounded-md shadow-md flex flex-col gap-6 2xl:w-1/3 dark:bg-dark-eval-1">
+            <div class="flex gap-4 justify-center items-center">
+                <h3 class="text-xl font-semibold text-center">TOP klientai</h3>
+                <a href="{{ route('admin-clients') }}"
+                    class="btn w-fit px-4 bg-sky-500 hover:bg-sky-600 flex items-center">Peržiūrėti
+                    visus</a>
+            </div>
             <x-back.home-top-clients name='Monika Levickaitė' orderAmount=12200 />
             <x-back.home-top-clients name='Kazys Bailiukas' orderAmount=8949.78 />
             <x-back.home-top-clients name='Marius Būžinskas' orderAmount=6499.99 />
