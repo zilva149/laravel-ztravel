9efece7f8930ec46db93bf650c8017af0dfa3ecf
zilva149 <114057257+zilva149@users.noreply.github.com>
2023-03-02 11:34:17 +0200

    reviews store method logic

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Order;
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ReviewController extends Controller
 {
     public function store(Request $request, Order $order) {
-        return 'making a review';
+        $incomingFields = $request->validate([
+            'rating' => ['required'],
+            'desc' => ['nullable'],
+            'user_id' => ['required', Rule::in(User::all()->pluck('id'))],
+            'order_id' => ['required', Rule::in(Order::all()->pluck('id'))],
+        ]);
+
+        $incomingFields['desc'] = strip_tags($incomingFields['desc']);
+        $incomingFields['user_id'] = auth()->user()->id;
+        $incomingFields['order_id'] = $order->id;
+
+        $order->update(['isReviewed' => '1']);
+        Review::create($incomingFields);
+
+        return redirect("/orders#$order->id")->with('success', 'Ačiū už atsiliepimą!')->with('id', $order->id);
     }
 
     public function update(Request $request, Review $review) {
--- database/migrations/2023_03_02_060716_create_reviews_table.php
+++ database/migrations/2023_03_02_060716_create_reviews_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->enum('rating', Review::RATING);
-            $table->text('desc');
+            $table->text('desc')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->timestamps();
