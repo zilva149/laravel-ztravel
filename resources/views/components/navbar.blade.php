435ab5e1d2441bd1e706d0458a41f5b9e1daa74f
zilva149 <114057257+zilva149@users.noreply.github.com>
2023-03-03 19:11:44 +0200

    admin navbar - done

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav aria-label="secondary" x-data="{ open: false }"
-    class="sticky top-0 z-10 flex items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white dark:bg-dark-eval-1"
+    class="sticky top-0 z-10 hidden md:flex items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white dark:bg-dark-eval-1"
     :class="{
         '-translate-y-full': scrollingDown,
         'translate-y-0': scrollingUp,
@@ -22,58 +22,68 @@
             <x-heroicon-o-sun x-show="isDarkMode" aria-hidden="true" class="w-6 h-6" />
         </x-button>
 
-        <x-dropdown align="right" width="48" class="hidden md:block">
-            <x-slot name="trigger">
-                <button
-                    class="flex items-center p-2 text-sm font-medium text-gray-500 rounded-md transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none focus:ring focus:ring-[var(--green)] focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-200">
-                    <div>{{ Auth::user()->name }}</div>
-
-                    <div class="ml-1">
-                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                </button>
-            </x-slot>
-
-            <x-slot name="content">
-                <!-- Profile -->
-                <x-dropdown-link :href="route('profile.edit')">
-                    {{ __('Paskyra') }}
-                </x-dropdown-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                        {{ __('Atsijungti') }}
+        <div class="hidden md:block">
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button
+                        class="flex items-center p-2 text-sm font-medium text-gray-500 rounded-md transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none focus:ring focus:ring-[var(--green)] focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-200">
+                        <div>{{ Auth::user()->name }}</div>
+    
+                        <div class="ml-1">
+                            <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+    
+                <x-slot name="content">
+                    <!-- Profile -->
+                    <x-dropdown-link :href="route('profile.edit')">
+                        {{ __('Paskyra') }}
                     </x-dropdown-link>
-                </form>
-            </x-slot>
-        </x-dropdown>
+    
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+    
+                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                            {{ __('Atsijungti') }}
+                        </x-dropdown-link>
+                    </form>
+                </x-slot>
+            </x-dropdown>
+        </div>
     </div>
 </nav>
 
 <!-- Mobile bottom bar -->
-<div class="fixed inset-x-0 bottom-0 flex items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white md:hidden dark:bg-dark-eval-1"
+<div class="sticky top-0 z-10 flex md:hidden items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white dark:bg-dark-eval-1"
     :class="{
-        'translate-y-full': scrollingDown,
+        '-translate-y-full': scrollingDown,
         'translate-y-0': scrollingUp,
     }">
-    <x-button type="button" icon-only variant="secondary" sr-text="Search">
-        <x-heroicon-o-search aria-hidden="true" class="w-6 h-6" />
-    </x-button>
+    <!-- Dark Mode Button -->
+    <div class="flex items-center gap-3">
+        <x-button type="button" class="md:hidden" icon-only variant="secondary" sr-text="Toggle dark mode"
+            x-on:click="toggleTheme">
+            <x-heroicon-o-moon x-show="!isDarkMode" aria-hidden="true" class="w-6 h-6" />
 
-    <a href="{{ route('index') }}">
-        <x-application-logo aria-hidden="true" class="w-10 h-10" />
+            <x-heroicon-o-sun x-show="isDarkMode" aria-hidden="true" class="w-6 h-6" />
+        </x-button>
+    </div>
 
-        <span class="sr-only">Dashboard</span>
+    <!-- Logo -->
+    <a href="{{ route('admin-home') }}" class="flex justify-center items-center">
+        <img src="{{ asset('assets/img/logo-full.png') }}" alt="ZTravel" aria-hidden="true" class="w-1/3">
+
+        <span class="sr-only">ZTravel</span>
     </a>
 
-    <x-button type="button" icon-only variant="secondary" sr-text="Open main menu"
+    <!-- Burger Menu -->
+    <x-button type="button" icon-only variant="primary" sr-text="Open main menu"
         x-on:click="isSidebarOpen = !isSidebarOpen">
         <x-heroicon-o-menu x-show="!isSidebarOpen" aria-hidden="true" class="w-6 h-6" />
 
